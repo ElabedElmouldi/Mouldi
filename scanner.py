0969d0be0498e759cd351c52710f68e31b214f0b
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-13 21:09:44 +0100

    Add scanning functionality to process signals

--- scanner.py
+++ scanner.py
@@ -1,1 +1,25 @@
+from data import get_symbols, get_data
 
+def scan(engine):
+
+    signals = []
+
+    for s in get_symbols():
+
+        try:
+            df = get_data(s)
+
+            result = engine.strategy.generate_signal(df)
+
+            if result["signal"]:
+
+                signals.append({
+                    "symbol": s,
+                    "price": df['c'].iloc[-1],
+                    "score": result["score"]
+                })
+
+        except:
+            continue
+
+    return signals
