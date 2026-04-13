5fd60bea5ffb70683b22967e4aa18395e0690478
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-13 21:06:53 +0100

    Implement message sending and command handling
    Added functions to send messages and handle commands for the Telegram bot.

--- telegram _bot.py
+++ telegram _bot.py
@@ -1,1 +1,21 @@
+import requests
+from config import BOT_TOKEN, CHAT_ID
+from portfolio import portfolio
 
+def send(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": text})
+
+
+def handle_command(text):
+
+    text = text.lower()
+
+    if text == "/balance":
+        send(f"💰 Balance: {portfolio['balance']}")
+
+    if text == "/positions":
+        send(str(portfolio["open_trades"]))
+
+    if text == "/start":
+        send("🤖 Bot Started")
