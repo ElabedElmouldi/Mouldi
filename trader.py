d02655e019100ede71410fbb279edba96d6b5e02
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-13 21:07:50 +0100

    Implement open_trade and update_trades functions

--- trader.py
+++ trader.py
@@ -1,1 +1,36 @@
 
+from portfolio import portfolio
+from config import RISK_PER_TRADE
+
+def open_trade(symbol, price, score):
+
+    if symbol in portfolio["open_trades"]:
+        return
+
+    size = portfolio["balance"] * RISK_PER_TRADE
+
+    portfolio["open_trades"][symbol] = {
+        "entry": price,
+        "tp": price * 1.02,
+        "sl": price * 0.99,
+        "size": size,
+        "score": score
+    }
+
+
+def update_trades(get_price, send):
+
+    for s in list(portfolio["open_trades"].keys()):
+
+        price = get_price(s)
+        trade = portfolio["open_trades"][s]
+
+        if price >= trade["tp"]:
+            portfolio["balance"] += trade["size"] * 0.02
+            send(f"🟢 TP {s}")
+            del portfolio["open_trades"][s]
+
+        elif price <= trade["sl"]:
+            portfolio["balance"] -= trade["size"] * 0.01
+            send(f"🔴 SL {s}")
+            del portfolio["open_trades"][s]
