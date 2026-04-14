745ed433ca1c889e55eb45ebd039e72690741e84
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-14 10:44:01 +0100

    Rename check_risk to risk_check and update logic

--- risk.py
+++ risk.py
@@ -1,6 +1,9 @@
-def check_risk(data):
+def risk_check(data):
 
-    if data["volatility"] > 2:
+    if data["volatility"] > 2.5:
+        return False
+
+    if data["rsi"] > 80 or data["rsi"] < 20:
         return False
 
     return True
