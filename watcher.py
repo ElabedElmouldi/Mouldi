ecaac6f3d9543d76c3d9c5728ac4cfcc293b585a
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-14 17:21:05 +0100

    Add functions to manage active watchlist

--- watcher.py
+++ watcher.py
@@ -1,1 +1,21 @@
 
+active_watchlist = {}
+
+def add_to_watchlist(coin):
+
+    active_watchlist[coin["symbol"]] = {
+        "entry": coin["price"],
+        "score": coin["score"]
+    }
+
+
+def update_watchlist(symbol, new_price):
+
+    if symbol not in active_watchlist:
+        return None
+
+    entry = active_watchlist[symbol]["entry"]
+
+    change = (new_price - entry) / entry * 100
+
+    return change
