17376b10213df466a83080678be645a40ce02123
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-14 09:32:56 +0100

    Add get_signal function for trading strategy

--- strategy.py
+++ strategy.py
@@ -1,1 +1,10 @@
+def get_signal(data):
 
+    score = data["momentum"] - data["volatility"]
+
+    if score > 0.5:
+        return "BUY"
+    elif score < -0.5:
+        return "SELL"
+
+    return "HOLD"
