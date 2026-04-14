da87cb6615f9224c0b839dba77882e42b324bd33
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-14 09:00:54 +0100

    Integrate Telegram API for message sending
    Updated send function to use Telegram API for messaging and added send_dashboard function to format dashboard messages.

--- notifications/telegram.py
+++ notifications/telegram.py
@@ -1,2 +1,23 @@
-def send(msg):
-    print("📲", msg)
+import requests
+
+BOT_TOKEN = "8439548325:AAHOBBHy7EwcX3J5neIaf6iJuSjyGJCuZ68"
+CHAT_ID = "5067771509"
+
+def send(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": text
+    })
+
+
+def send_dashboard(balance, trades_count):
+
+    text = f"""
+📊 DASHBOARD
+
+💰 Balance: {balance}
+📦 Open Trades: {trades_count}
+"""
+
+    send(text)
