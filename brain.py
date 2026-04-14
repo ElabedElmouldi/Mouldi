525aca9aae715e93b459cd7bac0d7c5fe05c2c17
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-14 10:46:20 +0100

    Add ai_score function to calculate score from data

--- brain.py
+++ brain.py
@@ -1,1 +1,12 @@
 
+def ai_score(data):
+
+    score = 0
+
+    score += data["trend"] * 2
+    score += data["momentum"] * 1.5
+    score += (data["rsi"] - 50) / 10
+    score += data["volume"]
+    score -= data["volatility"]
+
+    return score
