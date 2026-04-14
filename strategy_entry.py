659ef8b6ed11ef113db2bafd199b29bed680a8fa
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-14 21:04:18 +0100

    Add confirm_entry function for entry validation

--- strategy_entry.py
+++ strategy_entry.py
@@ -1,1 +1,15 @@
+def confirm_entry(data, change):
 
+    if change < 1.5:
+        return False
+
+    if data["volume"] < 1.5:
+        return False
+
+    if data["trend_5m"] != "UP":
+        return False
+
+    if data["volatility"] > 1.8:
+        return False
+
+    return True
