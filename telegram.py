4fa82e36025c54b9db26b0f8f555d7c35e63d06e
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-14 20:42:03 +0100

    Refactor send_message to send and improve error handling

--- telegram.py
+++ telegram.py
@@ -3,18 +3,22 @@
 import os
 import requests
 
-def send_message(text):
+def send(message):
 
-    token = os.getenv("8439548325:AAHOBBHy7EwcX3J5neIaf6iJuSjyGJCuZ68")   # ✅ variable name
-    chat_id = os.getenv("5067771509")   # ✅ variable name
+    token = os.getenv("8439548325:AAHOBBHy7EwcX3J5neIaf6iJuSjyGJCuZ68")
+    chat_id = os.getenv("5067771509")
 
     if not token or not chat_id:
-        print("Missing BOT_TOKEN or CHAT_ID")
+        print("❌ Missing BOT_TOKEN or CHAT_ID")
         return
 
     url = f"https://api.telegram.org/bot{token}/sendMessage"
 
-    requests.post(url, data={
-        "chat_id": chat_id,
-        "text": text
-    })
+    try:
+        requests.post(url, data={
+            "chat_id": chat_id,
+            "text": message
+        })
+        print("✅ Message sent")
+    except Exception as e:
+        print("❌ Telegram error:", e)
