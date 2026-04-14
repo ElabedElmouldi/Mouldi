3ea6281e3f862eb0c80292f7a65e42037aaf7cf0
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-14 10:41:00 +0100

    Fix duplicate send function in telegram.py
    Removed duplicate send function and ensured proper formatting.

--- notifications/telegram.py
+++ notifications/telegram.py
@@ -1,1 +1,27 @@
 
+
+import requests
+
+
+TOKEN = "YOUR_BOT_TOKEN"
+CHAT_ID = "YOUR_CHAT_ID
+
+def send(msg):
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": msg,
+        "parse_mode": "Markdown"
+    })
+
+def send(msg):
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": msg,
+        "parse_mode": "Markdown"
+    })
