1ffccdb6860003f57c0ef136d31496e86c87afdc
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-14 20:29:41 +0100

    Rename send function to send_message and update params

--- telegram.py
+++ telegram.py
@@ -1,21 +1,22 @@
 
 
+im
+
 import os
 import requests
 
-def send(msg):
+def send_message(text):
 
-    token = os.getenv("8439548325:AAHOBBHy7EwcX3J5neIaf6iJuSjyGJCuZ68")
-    chat = os.getenv("5067771509")
+    token = os.getenv("8439548325:AAHOBBHy7EwcX3J5neIaf6iJuSjyGJCuZ68")   # ✅ variable name
+    chat_id = os.getenv("5067771509")   # ✅ variable name
 
-    if not token or not chat:
+    if not token or not chat_id:
+        print("Missing BOT_TOKEN or CHAT_ID")
         return
 
-    requests.post(
-        f"https://api.telegram.org/bot{token}/sendMessage",
-        data={"chat_id": chat, "text": msg}
-    )
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
 
-
-
-
+    requests.post(url, data={
+        "chat_id": chat_id,
+        "text": text
+    })
