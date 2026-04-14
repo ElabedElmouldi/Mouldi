af411d70c77c5f9027682253640fba1ea0a5ab16
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-14 10:42:45 +0100

    Rename parameter in update method from 'result' to 'pnl'

--- portfolio.py
+++ portfolio.py
@@ -3,5 +3,5 @@ class Portfolio:
     def __init__(self):
         self.balance = 1000
 
-    def update(self, result):
-        self.balance += result
+    def update(self, pnl):
+        self.balance += pnl
